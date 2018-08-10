b5aafa441b07479628ef35ef1f481fb960931ce1
chrissx <35269695+chrissxYT@users.noreply.github.com>
2018-08-10 15:59:49 +0200

    Implemented a bit of encrypting CCrypt3.

--- CCrypt3/Program.cs
+++ CCrypt3/Program.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using LZ4PCL;
 using static System.Console;
 
 namespace CCrypt3
 {
-    class Program
+    static class Program
     {
+        static readonly int DEFAULT_BLOCK_SIZE = 1048576;
+        static readonly bool BIG_ENDIAN = !BitConverter.IsLittleEndian;
+        static readonly byte[] CCRYPT3_HEADER = new byte[8]
+        {
+          //C   C   R   Y   P   T   3 <EOT>
+            67, 67, 82, 89, 80, 84, 51, 4
+        };
+        static readonly byte[] IV = new byte[16]
+        {
+            //FO    CC    OF    FS    ON    OF    AB    IT
+            0xF0, 0xCC, 0x0F, 0xF5, 0x00, 0x0F, 0xAB, 0x17,
+            //CH    YO    US    OC    CT    HI    SI    S...
+            0xC0, 0x00, 0x05, 0x0C, 0xC7, 0x01, 0x51, 0x50
+            //yea...serious pseudo random mechanics
+        };
+
         static void Main(string[] args)
         {
             if(ReadKey().Key == ConsoleKey.E)
@@ -24,8 +44,21 @@ namespace CCrypt3
                         break;
                     ifs.Add(s);
                 }
-                //compress all the files into zstd
-                //encrypt the output with aes-256
+                FileStream ofs = File.Open(of, FileMode.Create);
+                byte[] key = SHA256.Create().ComputeHash(Encoding.Unicode.GetBytes(pw));
+                ICryptoTransform aes = new AesManaged().CreateEncryptor(key, IV);
+                byte[] bfr = new byte[DEFAULT_BLOCK_SIZE];
+                int count = -1;
+                ofs.w(CCRYPT3_HEADER);
+                ofs.w(bfr.Length);
+                foreach(string f in ifs)
+                {
+                    FileStream s = File.Open(f, FileMode.Open, FileAccess.Read);
+                    while ((count = s.Read(bfr, 0, bfr.Length)) > 0)
+                    {
+
+                    }
+                }
             }
             else
             {
@@ -33,11 +66,17 @@ namespace CCrypt3
             }
         }
 
-        //PLACEHOLDERS
-        //WILL BE REPLACED WITH CCMPR AND CCRYPTO FUNCTIONS
-        static byte[] comp(byte[] u) { return new byte[0]; }
-        static byte[] decomp(byte[] c) { return new byte[0]; }
-        static byte[] enc(byte[] d) { return new byte[0]; }
-        static byte[] dec(byte[] e) { return new byte[0]; }
+        static void w(this Stream s, params byte[] b)
+        {
+            s.Write(b, 0, b.Length);
+        }
+
+        static void w(this Stream s, int i)
+        {
+            byte[] b = BitConverter.GetBytes(i);
+            if (BIG_ENDIAN)
+                Array.Reverse(b);
+            s.Write(b, 0, 4);
+        }
     }
 }
